6a44cad8f12b403b7db14216868965356b9514e0
Fabian Peña <f.pena.jacobo@gmail.com>
2020-01-06 23:57:47 -0500

    FIX: fixtures data

--- api/models.py
+++ api/models.py
@@ -43,6 +43,7 @@ class Food(DateTable):
     food_category = models.ForeignKey(FoodCategory, on_delete=models.DO_NOTHING, null=False)
     name = models.CharField(max_length=100, null=False)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    image_pic = models.ImageField(default="backendrestaurantes/static/img/ajidegallina.jpg")
 
 
 class HistoricalPrice(DateTable):
