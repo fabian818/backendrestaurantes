cfada9719fa8f956206f47d1276dab5e99f339da
Alvaro Palacios Carrillo <apalacios@picap.co>
2020-01-09 17:40:25 -0500

    ADD: Detail view

--- api/food_tables/urls.py
+++ api/food_tables/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from api.food_tables.views import FoodTableList
-
+from api.food_tables.views import FoodTableList, FoodTableDetail
 
 urlpatterns = [
     path('', FoodTableList.as_view()),
+    path('<int:pk>', FoodTableDetail.as_view()),
 ]
