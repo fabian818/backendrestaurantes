f97e33a4328e81e1938c92f2784520eabb173194
Fabian Peña <f.pena.jacobo@gmail.com>
2020-01-13 13:59:38 -0500

    ADD: drf redoc documentation

--- backendrestaurantes/urls.py
+++ backendrestaurantes/urls.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BackendRestaurantes API",
+      default_version='v1',
+      description="Api for backendrestaurantes system",
+      contact=openapi.Contact(email="f.pena.jacobo@gmail"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
