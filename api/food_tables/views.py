28e4da5eed2aae56253ea65a4f31c147d10451f6
Alvaro Palacios Carrillo <apalacios@picap.co>
2020-01-15 21:10:24 -0500

    ADD: Filter and documentation to food tables

--- api/food_tables/views.py
+++ api/food_tables/views.py
@@ -1,15 +1,19 @@
 from rest_framework import generics
 from api.models import FoodTable
 from api.serializers import FoodTableSerializer
+from api.filters import FoodTableFilter
 
 
 class FoodTableList(generics.ListAPIView):
     """
     get:
-    Get tables with orders
+    Get tables with orders,
+    You can filter by table status as follows: /api/food_tables/?table_status_id__in=1
+    where the table_status_id must be: 1 (ready), 2 (occupied), 3 (deleted)
     """
     queryset = FoodTable.objects.all()
     serializer_class = FoodTableSerializer
+    filter_class = FoodTableFilter
 
 
 class FoodTableDetail(generics.RetrieveAPIView):
