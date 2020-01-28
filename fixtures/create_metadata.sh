16205202c8f2c16006936a2841fabb3c181e0143
Fabian Peña <f.pena.jacobo@gmail.com>
2020-01-28 16:22:30 -0500

    ADD: sale types fixtures for metadata

--- fixtures/create_metadata.sh
+++ fixtures/create_metadata.sh
@@ -4,4 +4,5 @@ python manage.py loaddata fixtures/meta_data/food_status.yaml
 python manage.py loaddata fixtures/meta_data/food_category.yaml
 python manage.py loaddata fixtures/meta_data/order_status.yaml
 python manage.py loaddata fixtures/meta_data/sale_status.yaml
+python manage.py loaddata fixtures/meta_data/sale_type.yaml
 python manage.py loaddata fixtures/meta_data/table_status.yaml
