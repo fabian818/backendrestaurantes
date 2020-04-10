6a04afbe69721de388e1883b8accf37169e109dd
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2020-04-10 16:03:04 -0500

    ADD: List + filters historical prices

--- api/api_admin/urls.py
+++ api/api_admin/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from api.api_admin.views import FoodCategoriesList, FoodsList, FoodCategoriesDetail, FoodsDetail
+from api.api_admin.views import FoodCategoriesList, FoodsList, FoodCategoriesDetail, FoodsDetail, HistoricalPricesList
 
 urlpatterns = [
     path('food_categories', FoodCategoriesList.as_view()),
     path('foods', FoodsList.as_view()),
     path('food_categories/<int:pk>/', FoodCategoriesDetail.as_view()),
-    path('foods/<int:pk>/', FoodsDetail.as_view())
+    path('foods/<int:pk>/', FoodsDetail.as_view()),
+    path('historical_prices', HistoricalPricesList.as_view()),
 ]
